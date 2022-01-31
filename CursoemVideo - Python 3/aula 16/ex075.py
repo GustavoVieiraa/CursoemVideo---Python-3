9b54d2a044aa9849e4d44a0a6ed876d9d99a034f
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-01-31 11:44:36 -0300

    ex075 finalizado

--- CursoemVideo - Python 3/aula 16/ex075.py
+++ CursoemVideo - Python 3/aula 16/ex075.py
@@ -3,15 +3,18 @@
 # B) Em que posição foi digitado o primeiro valor 3.
 # C) Quais foram os números pares.
 num0 = 0
-num1 = int(input('Informe um número: '))
-num2 = int(input('Informe um número outro: '))
-num3 = int(input('Informe um número outro: '))
-num4 = int(input('Informe um número outro: '))
-valores = (num1, num2, num3, num4)
-print(f'Os valores informados foram: {valores}')
-print(f'O número 9 apareceu [{valores.count(9)}x]')
-if num1 != 3 and num2 != 3 and num3 != 3 and num4 != 3:
-    print('O valor 3 não apareceu em nenhuma posição.')
+num = (int(input('Informe um número: ')),
+        int(input('Informe outro número: ')),
+        int(input('Informe mais um número: ')),
+        int(input('Informe o último número: ')))
+valores = (num)
+print(f'Você digitou os valores {num}')
+print(f'Você digitou {num.count(9)}x o número [9].')
+if 3 in num:
+    print(f'O número três apareceu na {num.index(3)+1}ª posição')
 else:
-    valores3 = (num0, num1, num2, num3, num4)
-    print(f'O número 3 apareceu na {valores3.index(3)}º posição')
+    print('O número três não apareceu em nenhuma posição.')
+print('Os valores pares são: ', end=' ')
+for n in num:
+    if n%2 == 0:
+        print(n, end=' ')
