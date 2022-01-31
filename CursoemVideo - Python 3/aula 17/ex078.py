beb2166627a500c4f8683d2e16d146104dc28e4e
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-01-31 20:28:53 -0300

    correção do ex078

--- CursoemVideo - Python 3/aula 17/ex078.py
+++ CursoemVideo - Python 3/aula 17/ex078.py
@@ -2,24 +2,33 @@
 #No Final, mostre qual foi o maior e o menor valor digitado 
 #e as suas respectivas posições na lista.
 '''                   # Variaveis Globais                   '''
-valores = list()
+valores = []
 maior = 0
-menor = 10000000000000000
+menor = 0
 '''                   # Back-end programa                    '''
 print('-=-' * 15)
 for cont in range(0, 5):
-    valores.append(int(input(' ' * 13 + f'|Posição| {cont} |Informe um valor: ')))
-    cont +=1
-for c, v in enumerate(valores):
-    print(c, v)
-    if v >= maior:
-        maior = v
-    if v <= menor:
-        menor = v
+    valores.append(int(input(' ' * 13 + f'|Posição| {cont+1} |Informe um valor: ')))
+    
+    if cont == 0:
+        maior = menor = valores[cont]
+    else:
+        if valores[cont] > maior:
+            maior = valores[cont]
+        if valores[cont] < menor:
+            menor = valores[cont]
 '''                     # Layout do Menu                    '''
 print('-=-' * 15)
 print('  ' * 8 + '*-* MENU *-*')
 print(f'Os valores digitados foram: {valores}')
-print(f'O maior valor digitado foi: [{maior}] na posição {c}')
-print(f'O menor valor digitado foi: [{menor}] na posição {c}')
+print(f'O maior valor digitado foi: [{maior}] na(s) posição(s)  ', end='')
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f'{i+1}... ', end='')
+print()
+print(f'O menor valor digitado foi: [{menor}] na(s) posição(s)  ', end='')
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f'{i+1}... ', end='')
+print()
 print('-=-' * 15)
