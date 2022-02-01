85e5a031aa54660eac2772d66a4e10fea720be14
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-02-01 00:25:10 -0300

    ex081 finalizado com sucesso

--- CursoemVideo - Python 3/aula 17/ex081.py
+++ CursoemVideo - Python 3/aula 17/ex081.py
@@ -3,6 +3,27 @@
     #A) Quantos números foram digitados.
     #B) A lista de valores, ordenada de forma decrescente.   
     #C) Se o valor 5 foi digitado e está ou não na lista.'''
-print('~~' * 30)
-print('')
-print('~~' * 30)
+print('~~' * 27)
+print(' ' * 13 + 'EXTRAINDO DADOS DE UMA LISTA')
+print('~~' * 27)
+lista = []
+'''                         VALIDAÇÃO DE CONTINUAÇÃO                       '''
+while True:
+    n = int(input('Digite um número: '))
+    lista.append(n)
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()
+    if resp == 'S':
+            print('--' * 27)
+    if resp == 'N':
+        break
+'''                         DESCRIÇÕES FINAIS                          '''
+print('--' * 27)
+print(f'Foram digitados {len(lista)}')
+lista.sort(reverse=True)
+print(f'Seus números digitados em sua lista em ordem decrescente é: {lista}')
+if 5 in lista:
+    print('O número 5 foi encontrado na lista.')
+else:
+    print('O número 5 não foi encontrado na lista.')
