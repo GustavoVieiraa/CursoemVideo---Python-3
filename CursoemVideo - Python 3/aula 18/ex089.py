af5c2672a2e9835943c27341af2be844c8451785
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-02-01 20:38:48 -0300

    inclusão ex089

---
+++ CursoemVideo - Python 3/aula 18/ex089.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia nome e duas notas de vários alunos
+# e guarde tudo em uma lista composta.
+# No final, mostre um boletim contendo a média de cada um e permita 
+# que o usuário possa mostrar as notas de cada aluno individualmente.
+print('=-=' * 20)
+print(' ' * 18 + 'Boletim escolar - 2022')
+print('=-=' * 20)
