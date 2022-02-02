e3e1b9fcf768f76a2d6f012bb62603c45d35a279
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-02-02 19:29:58 -0300

    resolução do ex089

--- CursoemVideo - Python 3/aula 18/ex089.py
+++ CursoemVideo - Python 3/aula 18/ex089.py
@@ -5,3 +5,32 @@
 print('=-=' * 20)
 print(' ' * 18 + 'Boletim escolar - 2022')
 print('=-=' * 20)
+lista_info = []
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    lista_info.append([nome, [nota1, nota2], media])
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).upper().strip()
+    if resp == 'S':
+        print('=-=' * 20)
+    if resp == 'N':
+        break
+print('=-=' * 25)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('=-=' * 25)
+for i, a in enumerate(lista_info):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+
+while True:
+    print('=-=' * 15)
+    oc = int(input('Mostrar notas de qual aluno?(999 interrompe): '))
+    if oc == 999:
+        print('FINALIZANDO... ')
+        break
+    if oc <= len(lista_info) - 1:
+        print(f'Notas de {lista_info[oc][0]} são {lista_info[oc][1]}')
+print('=-=' * 30)
